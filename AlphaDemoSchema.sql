e505290c40c281792e0f6fdab4a28972289c64bd
Low Yong Siang <lowyongsiang@gmail.com>
2018-09-17 23:44:23 +0800

    Delete wrong schema

--- AlphaDemoSchema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE book ( 
-	book_id CHAR(13) PRIMARY KEY, 
-	language VARCHAR(32) NOT NULL, 
-	price INTEGER NOT NULL, 
-	date_of_publication DATE,
-	book_name VARCHAR(64) NOT NULL,
-	format VARCHAR(16) NOT NULL
-);
